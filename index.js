d2b89048956885b50a512988590f4812761eebb9
Matt Dolly <>
2021-06-07 18:04:07 +0000

    TACO CAT

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 
 //b) Change all of the letters in the dna string to UPPERCASE, then print the result.
-let newDna = dna.trim().toUpperCase();
-dna = newDna
+dna = dna.trim().toUpperCase();
+dna = dna.replace("GCT", "AGG")
 
 //c) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
-
 console.log(dna);
+
+console.log(`${dna.slice(4,7).toLowerCase()}o ${dna.slice(dna.indexOf('CAT'),dna.indexOf('CAT')+3).toLowerCase()}`);
